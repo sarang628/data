cf2cf4b2ae5a001366be7073e05b63b74d15822f
SRYANG <sarang628@naver.com>
2023-11-27 13:41:46 +0800

    move alarm data

---
+++ AlarmType.kt
@@ -0,0 +1,7 @@
+package com.sryang.torang.data
+
+enum class AlarmType {
+    LIKE,
+    REPLY,
+    FOLLOW
+}
---
+++ AlarmUser.kt
@@ -0,0 +1,5 @@
+package com.sryang.torang.data
+
+data class User(
+    val name: String
+)
